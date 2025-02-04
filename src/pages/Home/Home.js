b9c9d787b685e509356d6bebb175d63bfcd8a674
Lasse Hægland <seilenos@gmail.com>
2025-02-04 11:23:00 +0100

    Added some quotes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,9 +9,26 @@ function Home() {
           <h1>Welcome to the Rick and Morty Universe</h1>
           <Link to="/characters" className={styles.cta}>Explore Characters</Link>      </div>
       </div>
-      <main>
+      <main className={styles.main}>
         <h2>Lorem Ipsum</h2>
-        <p>Dill</p>
+        <p>
+          You're missing the point Morty. Why would he drive a smaller toaster with wheels? I mean, does your car look like a smaller version of your house? No.
+        </p>
+        <p>
+          It's like the N word and the C word had a baby and it was raised by all the bad words for Jews.
+        </p>
+        <p>
+          Morty, do you know what “wubba lubba dub dub” means?.
+        </p>
+        <p>
+          What about the reality where Hitler cured cancer, Morty? The answer is: Don't think about it.
+        </p>
+        <p>
+          Weddings are basically funerals with cake.
+        </p>
+        <p>
+          I dunno, some people would pay top dollar for that kind of
+        </p>
       </main>
     </>
   );
